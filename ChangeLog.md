420f290d2fca4653b3c441648b471293aeffa3b1
lazzlo2096 <lazzlo2096@gmail.com>
2020-05-06 20:05:54 +0300

    add some code;

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for fdupes-xenon
+
+## Unreleased changes
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
+--import Lib
 
-import Lib
+import qualified Data.ByteString.Lazy as LB
+import Crypto.Hash
+
+--md5 :: LB.ByteString -> Digest MD5
+--md5 = hashlazy
 
 main :: IO ()
-main = someFunc
+main = do
+    fileContent <- LB.readFile "foo.txt"
+    let md5Digest = hashlazy fileContent :: Digest MD5
+    -- let md5Digest = md5 fileContent
+    print $ digestToHexByteString md5Digest
+
+    --LB.readFile "foo.txt" >>= print . digestToHexByteString . (hashlazy :: LB.ByteString -> Digest MD5)
